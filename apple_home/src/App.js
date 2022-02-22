72d2e516f4fd85957d83d1ee7cbe1b45cda96552
Samuel Dorismond <samueldorismond@MacBook-Air-de-Samuel.local>
2022-02-22 11:47:36 +0100

    ok

---
+++ apple_home/src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
+import IphoneStart from './components/Iphone-Start';
+import FilmApple from './components/Film-Apple';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <IphoneStart />
+      <FilmApple />
+    </div>
+  );
+}
+
+export default App;
